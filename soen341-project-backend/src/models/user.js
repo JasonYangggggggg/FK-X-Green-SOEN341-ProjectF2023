fb8c4e0810ec2710c8ce89678f11c26b3e660f63
Jason (Zhuohong) Yang <109561939+JasonYangggggggg@users.noreply.github.com>
2023-11-10 13:28:33 -0500

    Add files via upload

---
+++ soen341-project-backend/src/models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+    Name: { type: String, required: true },
+    Password: { type : String, required: true, trim: true } 
+}, {
+    timestamps : true
+});
+
+userSchema.pre('save', async function(next){
+    const user = this;
+    if(user.isModified('Password')){
+        user.Password = await bcrypt.hash(user.Password, 8);
+    }
+    next();
+});
+
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
