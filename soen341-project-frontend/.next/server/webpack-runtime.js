e75d6bc199005cd00cbc415a8f5a03dce20ab340
AlimuratDinch <alimrtdinch312@gmail.com>
2023-11-29 18:30:20 -0500

    addtiion

--- soen341-project-frontend/.next/server/webpack-runtime.js
+++ soen341-project-frontend/.next/server/webpack-runtime.js
@@ -47,6 +47,11 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	(() => {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -125,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("97042e75f6bde380")
+/******/ 		__webpack_require__.h = () => ("b8917aa8773ec736")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- soen341-project-frontend/.next/static/chunks/webpack.js
+++ soen341-project-frontend/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e2876579dc039e0a"; }
+/******/ 		__webpack_require__.h = function() { return "7820de136e7c3db7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
